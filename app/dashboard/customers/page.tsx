db87121b6166296a744c13084d086ac319529562
chocoanish <chocoboyanish566@gmail.com>
2024-12-26 00:45:35 +0530

    Metadata and Project Completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next"
+
+export const meta: Metadata = {
+    title: "Customers",
+    description: "Customers page",
+    keywords: "customers, clients, users",
+}
+
 export default function Page(){
     return <p>Customer Page</p>
 }
--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,13 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
- 
+import { Metadata } from 'next'; 
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+  description: 'Edit an existing invoice',
+};
+
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
     const id = params.id;
